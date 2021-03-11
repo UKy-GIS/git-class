637a2444bee591702b06a82aadc23cb49eb719a8
Boyd <boyd.land@gmail.com>
2021-03-11 14:32:17 -0500

    add comments

--- clone.sh
+++ clone.sh
@@ -5,18 +5,19 @@ class=uky-gis
 mod=geo409-mod-03
 folder=/Volumes/D/_repos/UKy-GIS/_student/_$mod
 
-# Make directory and clone repos
+# Make directory
 mkdir $folder
-cp list.txt $folder/
-cd $folder
 
+# Copy list of Github usernames, one name per line
+cp list.txt $folder/
+
+# Move into directoruy and clone repos
+cd $folder
 for x in $(cat list.txt)
     do
     repo=https://www.github.com/$class/$mod-$x.git
-
     remote=$(git ls-remote -h ${repo})
     isLocal=$(ls ${mod}-${x} > /dev/null) 
-
     if [[ -z ${remote} ]]; then
         echo "NO ************ $x ************"
     else
